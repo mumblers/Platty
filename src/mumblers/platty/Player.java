6e9fdab94bcc1135323591ed34c69b23e3322ebe
sinius15 <sinius15@gmail.com>
2015-08-23 11:01:38 +0200

    Adding MovementStatus

--- src/mumblers/platty/Player.java
+++ src/mumblers/platty/Player.java
@@ -4,4 +4,8 @@ package mumblers.platty;
  * Created by Sinius15 on 23-8-2015.
  */
 public class Player {
+
+    Direction direction;
+
+
 }
--- src/mumblers/platty/PlayerSprite.java
+++ src/mumblers/platty/PlayerSprite.java
@@ -1,7 +1,33 @@
 package mumblers.platty;
 
+import mumblers.platty.graphics.Sprite;
+
+import java.awt.*;
+
 /**
  * Created by Sinius15 on 23-8-2015.
  */
 public class PlayerSprite extends Sprite {
+
+    private Player thePlayer;
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public void render(Graphics2D g, int x, int y, int width, int height) {
+
+    }
+
+    @Override
+    public void renderRotated(Graphics2D g, int x, int y, int angle, double xScale, double yScale, int xOff, int yOff) {
+
+    }
 }
